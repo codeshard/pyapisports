4e7bc3db7540511862879993c6d5b091fc6d0948
Ozkar L. Garcell <ozkar.garcell@gmail.com>
2026-03-01 22:58:02 +0100

    feat: added global imports

--- src/pyapisports/models/__init__.py
+++ src/pyapisports/models/__init__.py
@@ -1,5 +1,13 @@
 from .country import Country, CountryList
-from .status import Status
+from .status import Account, Requests, Status, Subscription
 from .timezone import TimezoneList
 
-__all__ = ["Status", "TimezoneList", "Country", "CountryList"]
+__all__ = [
+    "Status",
+    "TimezoneList",
+    "Country",
+    "CountryList",
+    "Account",
+    "Requests",
+    "Subscription",
+]
