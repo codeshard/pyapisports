5dcbf6235272a33a88093170891cdb619c5a71af
Ozkar L. Garcell <ozkar.garcell@gmail.com>
2026-03-17 21:50:22 +0100

    wip: football resource

--- src/pyapisports/resources/football.py
+++ src/pyapisports/resources/football.py
@@ -1,4 +1,18 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from pyapisports.models import SeasonsList
 from pyapisports.resources import BaseResource
 
+if TYPE_CHECKING:
+    from pyapisports.client import ApiSportsClient
 
-class FootballResource(BaseResource): ...
+
+class FootballResource(BaseResource):
+    def __init__(self, client: ApiSportsClient):
+        self._client = client
+
+    def get_seasons(self) -> SeasonsList:
+        raw = self._client._get("/leagues/seasons")
+        return SeasonsList.from_api(raw)
