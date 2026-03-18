2ccfe7ec59a8851d7bf39fe93841800546cc1d72
Ozkar L. Garcell <ozkar.garcell@gmail.com>
2026-03-18 19:41:42 +0100

    wip: football resources

--- src/pyapisports/resources/football.py
+++ src/pyapisports/resources/football.py
@@ -1,8 +1,8 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any
 
-from pyapisports.models import SeasonsList
+from pyapisports.models import LeagueList, SeasonsList
 from pyapisports.resources import BaseResource
 
 if TYPE_CHECKING:
@@ -16,3 +16,32 @@ class FootballResource(BaseResource):
     def get_seasons(self) -> SeasonsList:
         raw = self._client._get("/leagues/seasons")
         return SeasonsList.from_api(raw)
+
+    def get_leagues(
+        self,
+        id: int | None = None,
+        name: str | None = None,
+        country: str | None = None,
+        code: str | None = None,
+        season: int | None = None,
+        current: bool | None = None,
+        search: str | None = None,
+    ) -> LeagueList:
+        params: dict[str, Any] = {}
+        if id:
+            params["id"] = id
+        if name:
+            params["name"] = name
+        if country:
+            params["country"] = country
+        if code:
+            params["code"] = code
+        if season:
+            params["season"] = season
+        if current is not None:
+            params["current"] = "true" if current else "false"
+        if search:
+            params["search"] = search
+
+        raw = self._client._get("/leagues", params=params)
+        return LeagueList.from_api(raw)
