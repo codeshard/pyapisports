278b6f43ecf6fa1f9ff2773f62105118b999edd5
Ozkar L. Garcell <ozkar.garcell@gmail.com>
2026-03-29 00:13:12 +0100

    wip: teams models

--- src/pyapisports/models/teams.py
+++ src/pyapisports/models/teams.py
@@ -1,4 +1,6 @@
+import json
 from dataclasses import dataclass
+from typing import Any
 
 
 @dataclass
@@ -10,3 +12,29 @@ class Team:
     founded: int
     national: bool
     logo: str
+
+    @classmethod
+    def from_api(cls, data: dict[str, Any]) -> "Team":
+        return cls(
+            id=data["id"],
+            name=data["name"],
+            code=data["code"],
+            country=data["country"],
+            founded=data["founded"],
+            national=data["national"],
+            logo=data["logo"],
+        )
+
+    def to_dict(self) -> dict[str, Any]:
+        return {
+            "id": self.id,
+            "name": self.name,
+            "code": self.code,
+            "country": self.code,
+            "founded": self.founded,
+            "national": self.national,
+            "logo": self.logo,
+        }
+
+    def to_json(self, **kwargs: Any) -> str:
+        return json.dumps(self.to_dict(), **kwargs)
